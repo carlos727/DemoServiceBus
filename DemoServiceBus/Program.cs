e8e168b9e6f749dfe0c3d19bffe32e5237f72a85
Carlos Beleño <cbeleno@redsis.com>
2018-03-16 14:53:04 -0500

    first commit

---
+++ DemoServiceBus/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace SenderApp
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://evaapp.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=KkEqOUkJD4XQoq4W04D7Lyg5P/Sq1FQg2B1KUYMf45w=";
+        const string QueueName = "myqueue";
+        static IQueueClient queueClient;
+
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after sending all the messages.");
+            Console.WriteLine("======================================================");
+
+            // Send messages.
+            await SendMessagesAsync("Sema ++");
+
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+        }
+
+        static async Task SendMessagesAsync(string text)
+        {
+            try
+            {
+                string messageBody = $"{text}";
+                var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                // Write the body of the message to the console.
+                Console.WriteLine($"Sending message: {messageBody}");
+
+                // Send the message to the queue.
+                await queueClient.SendAsync(message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+    }
+}
